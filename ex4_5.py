aed33dd1a818162b21da26087d5a6992c56d8887
sasaki0719 <y.sasaki.roppongi@gmail.com>
2023-11-15 11:19:53 +0900

    dictionaryを使う方法にコード4-5修正

--- ex4_5.py
+++ ex4_5.py
@@ -1,29 +1,31 @@
-temp = [7.8, 9.1, 10.2, 11.0, 12.5, 12.4, 14.3, 13.8, 12.9, 12.4]
+# temp = [7.8, 9.1, 10.2, 11.0, 12.5, 12.4, 14.3, 13.8, 12.9, 12.4]
 # 1
-# temp = []
-# hour = 8
-# for h in range(10):
-#     t = input(f"{hour}時の気温(℃)を入力 >>")
-#     temp.append(t)
-#     hour += 1
+temp = {}
+hour = 8
+for key in range(10):
+    t = float(input(f"{hour}時の気温(℃)を入力 >>"))
+    temp[hour] = t
+    hour += 1
 
 # 2
-for data in temp:
-    print(data)
+for data in temp.values():
+    print(data,end=", ")
+print()
 
 # 3
-temp_new = []
-for h in range(len(temp)):
-    if h == 5:
-        temp_new.append("N/A")
+temp_new = {}
+for key in temp:
+    if key == 13:
+        temp_new[key] = "N/A"
     else:
-        temp_new.append(temp[h])
+        temp_new[key] = temp[key]
 print(temp)
 print(temp_new)
+print()
 
 # 4
 total = 0
-for data in temp_new:
+for data in temp_new.values():
     if isinstance(data,str):
         continue
     total += data
