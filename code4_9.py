18a9e60612da587659be789083fdfa92f6b402ac
sasaki0719 <y.sasaki.roppongi@gmail.com>
2023-11-15 10:08:42 +0900

    教科書4章 コード4-11まで

---
+++ code4_9.py
@@ -0,0 +1,11 @@
+ages = [28,50,8,20,78,25,22,10,27,33]
+num = 5
+samples = list()
+
+for age in ages:
+    if 20 <= age < 30:
+        if len(samples) < num:
+            samples.append(age)
+            if len(samples) == num:
+                break
+print(samples)
